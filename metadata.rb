f8b43120fc72d10a050be7da540b29ff926f3077
Mike Moate <github@mikemoate.co.uk>
2016-05-06 13:54:14 +0100

    Add cookbook dependencies
    A bit premature, but I know we'll be using these so easier than keeping notes.

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license 'all_rights'
 description 'Installs/Configures mac_bootstrap'
 long_description 'Installs/Configures mac_bootstrap'
 version '0.1.0'
+
+supports 'mac_os_x'
+
+depends 'build-essential', '~> 3.2.0'
+depends 'homebrew' '~> 2.1.0'
