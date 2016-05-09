0be7d18428795e3ec35e0e11c48e2d10e074036f
Mike Moate <github@mikemoate.co.uk>
2016-05-09 21:49:27 +0100

    Fix indentation, replace tabs with 2 spaces.

--- mac_bootstrap.sh
+++ mac_bootstrap.sh
@@ -20,53 +20,53 @@ xcode-select -p &> /dev/null
 if [ $? == 2 ]
 then
   echo
-	echo "${RED}  Installing Xcode Command Line Tools ...${RESET}"
-	echo "${RED}  ---------------------------------------${RESET}"
-	
-	# Taken from https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
+  echo "${RED}  Installing Xcode Command Line Tools ...${RESET}"
+  echo "${RED}  ---------------------------------------${RESET}"
+  
+  # Taken from https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
 
-	# create the placeholder file that's checked by CLI updates' .dist code 
-	# in Apple's SUS catalog
-	touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-	# find the CLI Tools update
-	PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-	# install it
-	softwareupdate -i "$PROD" -v
+  # create the placeholder file that's checked by CLI updates' .dist code 
+  # in Apple's SUS catalog
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  # find the CLI Tools update
+  PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+  # install it
+  softwareupdate -i "$PROD" -v
 else
-	echo
-	echo "${GREEN}  Xcode Command Line Tools already installed.${RESET}"	
+  echo
+  echo "${GREEN}  Xcode Command Line Tools already installed.${RESET}"  
 fi
 
 if [ "$(whoami)" != "$(stat -f '%Su' /usr/local/bin/)" ]
 then
-	echo
-	echo "${RED}  Fixing ownership of /usr/local/ ...${RESET}"
-	echo "${RED}  -----------------------------------${RESET}"
-	sudo chown -R $(whoami):admin /usr/local
+  echo
+  echo "${RED}  Fixing ownership of /usr/local/ ...${RESET}"
+  echo "${RED}  -----------------------------------${RESET}"
+  sudo chown -R $(whoami):admin /usr/local
 else
-	echo
-	echo "${GREEN}  Owner of /usr/local/bin is already $(whoami).${RESET}"
+  echo
+  echo "${GREEN}  Owner of /usr/local/bin is already $(whoami).${RESET}"
 fi
 
 if [ ! -f "/opt/chef/bin/chef-client" ]
 then
-	echo
-	echo "${RED}  Installing Chef ...${RESET}"
-	echo "${RED}  -------------------${RESET}"
-	curl -L https://omnitruck.chef.io/install.sh | sudo bash
+  echo
+  echo "${RED}  Installing Chef ...${RESET}"
+  echo "${RED}  -------------------${RESET}"
+  curl -L https://omnitruck.chef.io/install.sh | sudo bash
 else
-	echo
-	echo "${GREEN}  Chef already installed.${RESET}"
+  echo
+  echo "${GREEN}  Chef already installed.${RESET}"
 fi
 
 if [ ! -f "/opt/chef/embedded/bin/berks" ]
 then
-	echo
-	echo "${RED}  Installing Berkshelf for Chef ...${RESET}"
-	echo "${RED}  ---------------------------------${RESET}"
-	sudo /opt/chef/embedded/bin/gem install --no-rdoc --no-ri berkshelf
-	ln -s /opt/chef/embedded/bin/berks /usr/local/bin
+  echo
+  echo "${RED}  Installing Berkshelf for Chef ...${RESET}"
+  echo "${RED}  ---------------------------------${RESET}"
+  sudo /opt/chef/embedded/bin/gem install --no-rdoc --no-ri berkshelf
+  ln -s /opt/chef/embedded/bin/berks /usr/local/bin
 else
-	echo
-	echo "${GREEN}  Berkshelf already installed.${RESET}"
+  echo
+  echo "${GREEN}  Berkshelf already installed.${RESET}"
 fi
