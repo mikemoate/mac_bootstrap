375ff40d73cab5ad1815b203125d0fe2a306af3a
Mike Moate <github@mikemoate.co.uk>
2016-05-11 00:33:59 +0100

    Fix up metadata and add basic README

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # mac_bootstrap
 
+## Platform
+- Mac OS X (10.11+)
+
+## Cookbooks
+- homebrew: for package management on OS X
+
+## Usage
 ```bash
 curl -L https://raw.githubusercontent.com/mikemoate/mac_bootstrap/master/mac_bootstrap.sh | bash
 ```
--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,11 @@
 name 'mac_bootstrap'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'Mike Moate'
+maintainer_email 'chef@mikemoate.co.uk'
+license 'MIT'
 description 'Installs/Configures mac_bootstrap'
-long_description 'Installs/Configures mac_bootstrap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 supports 'mac_os_x'
 
-depends 'build-essential', '~> 3.2.0'
 depends 'homebrew', '~> 2.1.0'
