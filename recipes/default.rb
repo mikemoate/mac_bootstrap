1c2373ed82555bf5b705e2d6d0f5db452cfb8846
Mike Moate <github@mikemoate.co.uk>
2016-05-15 22:49:55 +0100

    Add Homebrew Cask Versions tap
    The Versions tap has casks for some newer software (e.g. Sublime Text 3).
    
    Install this so we have access to these versions.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,9 @@ include_recipe "homebrew::default"
 # Install Homebrew Cask tap
 include_recipe "homebrew::cask"
 
+# Install Homebrew Cask Versions tap
+homebrew_tap 'caskroom/versions'
+
 #Install formulae/packages (list is defined in attributes)
 include_recipe "homebrew::install_formulas"
 
